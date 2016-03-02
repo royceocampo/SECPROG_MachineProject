916907b477052bda9554c52c8b7f273184ce3ded
jval0 <jval0@users.noreply.github.com>
2016-03-02 12:07:59 +0800

    porcupine

---
+++ FoobarBookshopMP/src/edu/secprog/dto/Administrator.java
@@ -0,0 +1,34 @@
+package edu.secprog.dto;
+
+public class Administrator extends Account {
+	private String username;
+	private String password;
+	
+	
+	public Administratorr(){}
+	
+	public Administrator(String username, String password){
+	  
+		this.username = username;
+		this.password= password;
+	}
+	
+	public String getUsername() {
+		return username;
+	}
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public void setPassword(String username){
+		this.password = password;
+	}
+	
+	//more methods frickin needed
+	
+}
